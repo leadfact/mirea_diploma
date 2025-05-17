d8f351d141ea4b1eaca9037f3f719dc1584888ab
Derdzyan <derca@bwg-io.site>
2025-05-17 14:03:26 +0300

    a

--- infra/zabbix/zabbix-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Create the directory and set permissions
-mkdir -p /var/run/zabbix || { echo "Failed to create /var/run/zabbix"; exit 1; }
-chown zabbix:zabbix /var/run/zabbix || { echo "Failed to chown /var/run/zabbix"; exit 1; }
-chmod 755 /var/run/zabbix
-# Run Zabbix server as the zabbix user
-exec gosu zabbix /usr/sbin/zabbix_server -f
