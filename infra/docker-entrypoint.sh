fda1672502b84a01951ef6719d7bf1f7eda26f21
Derdzyan <derca@bwg-io.site>
2025-05-17 11:39:40 +0300

    a

--- infra/docker-entrypoint.sh
+++ infra/docker-entrypoint.sh
@@ -7,4 +7,4 @@ chown -R zabbix:zabbix /var/run/zabbix /var/log/zabbix
 chmod 755 /var/run/zabbix /var/log/zabbix
 
 # Execute the command as zabbix user
-exec su-exec zabbix "$@" 
+exec su - zabbix -c "$@" 
